12416328cf682341c7ea2583eaf84b240ea9cea4
Angelo <Angelocool177@gmail.com>
2018-05-12 01:31:46 +0800

    Added codes in tests

--- djangopoll/polls/models.py
+++ djangopoll/polls/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 
@@ -7,8 +9,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,
+                                 related_name='choices')
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
